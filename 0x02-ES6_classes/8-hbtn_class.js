ff763e0a1bdcf1c153c1ec1c49aa48abbc7e1823
James gitere <sasmuypwa2019.com>
2024-06-27 19:23:39 +0300

    class js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(s) {
+    this._size = s;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(l) {
+    this._location = l;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
