37d188edede638a61488619e6c817b6dd85cc837
James gitere <sasmuypwa2019.com>
2024-06-27 14:02:13 +0300

    javascript classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amt) {
+    this._amount = amt;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} currency_name (${this._currency})`;
+  }
+
+  static convertPrice(amount, convertionRate) {
+    return amount * convertionRate;
+  }
+}
