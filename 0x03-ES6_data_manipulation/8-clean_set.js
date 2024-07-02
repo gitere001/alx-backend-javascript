0e3e8a6d57c54bb1e1d73efac81bbbcc1a95d7ba
James gitere <sasmuypwa2019.com>
2024-07-02 11:14:36 +0300

    added documentation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -7,18 +7,17 @@
  * @return {string} The cleaned set values joined by a hyphen.
  */
 export default function cleanSet(set, startString) {
-  const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  for (const value of set.values()) {
-    if (typeof value === 'string' && value.startsWith(startString)) {
-      const valueSubStr = value.substring(startString.length);
 
-      if (valueSubStr && valueSubStr !== value) {
-        parts.push(valueSubStr);
-      }
+  const result = [];
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
     }
-  }
-  return parts.join('-');
+  });
+
+  return result.join('-');
 }
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -4,12 +4,14 @@
  * @return {Map} The grocery list as a Map.
  */
 export default function groceriesList() {
-  const values = [
+  const groceryList = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-  return new Map(values);
+
+  const groceryMap = new Map(groceryList);
+  return groceryMap;
 }
