ebca066d702255bffe8dd480ad89f05fb355928e
James gitere <sasmuypwa2019.com>
2024-07-02 10:15:01 +0300

    cleaning a set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,26 +1,21 @@
-export default function cleanSet(set, string) {
-  if (!set || !string) {
+/**
+ * Cleans a set by removing the starting string from each value and
+ * joining the results with a hyphen.
+ *
+ * @param {Set} set - The set to be cleaned.
+ * @param {string} startString - The starting string to remove from each value in the set.
+ * @return {string} The cleaned set values joined by a hyphen.
+ */
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  if (typeof set[Symbol.iterator] !== 'function') {
-    throw new TypeError('Input set must be iterable');
-  }
-  if (typeof string !== 'string') {
-    throw new TypeError('Input string must be a string');
-  }
-  if (string === '') {
-    return '';
-  }
-  const cleanedArray = [];
-  for (const i of set) {
-    if (typeof i !== 'string') {
-      throw new TypeError('Input set must contain strings');
+  const cleanSetArray = [];
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      const restString = value.slice(startString.length);
+      cleanSetArray.push(restString);
     }
-    if (i.includes(string)) {
-      const result = i.replace(string, '');
-      cleanedArray.push(result);
-    }
-  }
-  const joinedSting = cleanedArray.join('-');
-  return joinedSting;
+  });
+  return cleanSetArray.join('-');
 }
