842aab7248b6d9278643af62fa95ed846d895587
James gitere <sasmuypwa2019.com>
2024-07-02 10:31:56 +0300

    map in js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const groceryList = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  const groceryMap = new Map(groceryList);
+  return groceryMap;
+}
