4d50035446f3c228784008ef7c9493da464a0730
James gitere <sasmuypwa2019.com>
2024-07-02 12:42:51 +0300

    set and array

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,10 @@
+/**
+ * Updates the values in the given Map to 100 if their corresponding value is 1.
+ *
+ * @param {Map} map - The Map to be updated.
+ * @return {void} This function does not return anything.
+ * @throws {Error} If the input is not an instance of Map.
+ */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
