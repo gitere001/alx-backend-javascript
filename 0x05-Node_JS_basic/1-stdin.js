cde7aea7cecc46ead1991cdcd4f0adba7d4e288c
James gitere <sasmuypwa2019.com>
2024-08-20 08:36:00 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-const process = require('process');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  console.log('Your name is:', data.toString().trim());
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`Your name is ${data.toString().trim()}\n`);
   process.exit();
+});
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
