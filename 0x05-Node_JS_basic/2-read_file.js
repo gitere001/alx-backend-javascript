057b2ceb108ef1688fc8c2da27dc945cf3c4a557
James gitere <sasmuypwa2019.com>
2024-08-20 11:38:02 +0300

    reading file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,30 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  if (fs.existsSync(path)) {
+  if (fs.existsSync(path) && (fs.statSync(path).isFile())) {
     const data = fs.readFileSync(path, 'utf8');
-    if (data.length > 0) {
-      const lines = data.split('\n');
-      const students = lines.slice(1).filter((line) => line.trim() !== '');
-      let noOfStudents = 0;
-      const studentFields = {};
-
-      students.forEach((line) => {
-        const parts = line.trim().split(',');
-        if (parts.length === 4) {
-          const name = parts[0];
-          const field = parts[3];
-          if (!studentFields[field]) {
-            studentFields[field] = [];
-          }
-          studentFields[field].push(name);
-          noOfStudents += 1;
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
+    let noOfStudents = 0;
+    const studentFields = {};
+    students.forEach((line) => {
+      const parts = line.trim().split(',');
+      if (parts.length === 4) {
+        const name = parts[0];
+        const field = parts[3];
+        if (!studentFields[field]) {
+          studentFields[field] = [];
         }
-      });
-
-      console.log(`Number of students: ${noOfStudents}`);
-      Object.keys(studentFields).forEach((field) => {
-        const names = studentFields[field];
-        console.log(`Number of students in ${field}: ${names.length} List: ${names.join(', ')}`);
-      });
-    } else {
-      console.log('Cannot load the database');
+        studentFields[field].push(name);
+        noOfStudents += 1;
+      }
+    });
+    console.log(`Number of students: ${noOfStudents}`);
+    for (const [field, names] of Object.entries(studentFields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
   } else {
-    console.log('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 };
-
 module.exports = countStudents;
