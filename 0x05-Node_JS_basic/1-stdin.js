71bb806ec18e48f3699a1fa372e7612abfd34b1f
James gitere <sasmuypwa2019.com>
2024-08-20 08:53:47 +0300

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-    process.exit();
-  });
-}
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.exit(); // Ends the process after displaying the name
+});
 
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
